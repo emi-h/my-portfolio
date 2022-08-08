8b706e252b651eaa14fb6fea530f9475b088b5f7
emih <emi.programming@gmail.com>
2022-08-08 12:53:30 +0900

    ✅ Add footer component

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Footer.module.css'
+
+export const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <small>&copy; 2022 Emi H</small>
+        </footer>
+    )
+}
