eb45bd59651c91722524c509a5ef91e4c1b616a4
emih <emi.programming@gmail.com>
2022-08-10 16:01:53 +0900

    ✨ Embed twitter timeline

--- components/Twitter.tsx
+++ components/Twitter.tsx
@@ -1,3 +1,4 @@
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
 import styles from '../styles/Twitter.module.css'
 import { Btn } from './Btn'
 
@@ -6,18 +7,18 @@ export const Twitter = () => {
         <section className={styles.twitter} id="twitter">
             <h2>twitter</h2>
             <div className={styles.twitter_body}>
-                <ul>
-                    <li>
-                        <a href="#">
-                            tweet
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            tweet
-                        </a>
-                    </li>
-                </ul>
+                <div className={styles.twitter_box}>
+                    <TwitterTimelineEmbed
+                        onLoad={function noRefCheck() { }}
+                        options={{
+                            height: 810
+                        }}
+                        sourceType="profile"
+                        userId={'1254763822050467840'}
+                    />
+                    {/* <a class="twitter-timeline" data-height="500" data-theme="dark" href="https://twitter.com/emi11882681?ref_src=twsrc%5Etfw">Tweets by emi11882681</a>
+                    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> */}
+                </div>
             </div>
             <Btn text='View on Twitter' url='https://twitter.com/emi11882681' />
         </section>
