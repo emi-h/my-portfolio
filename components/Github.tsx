0db238403ce9678829f2212bf097ed5855209d95
emih <emi.programming@gmail.com>
2022-08-10 17:15:49 +0900

    💄 Add styles

--- components/Github.tsx
+++ components/Github.tsx
@@ -1,4 +1,7 @@
+import Image from 'next/image'
 import styles from '../styles/GitHub.module.css'
+import StarImg from '../public/i_star.svg'
+import BranchImg from '../public/i_branch.svg'
 import { Btn } from './Btn'
 
 export const GitHub = () => {
@@ -11,22 +14,190 @@ export const GitHub = () => {
                         <a href="#">
                             <h3>lightsound/nexst-tailwind</h3>
                             <p>Next.js starter template.</p>
-                            <div>
-                                <span>★</span><span>117</span><span>★</span><span>18</span>
+                            <div className={styles.icons}>
+                                <div className={styles.icon}>
+                                    <span>
+                                        <Image
+                                            src={StarImg}
+                                            alt="star"
+                                        />
+                                    </span>
+                                    <span>117</span>
+                                </div>
+                                <div className={styles.icon}>
+                                    <span>
+                                        <Image
+                                            src={BranchImg}
+                                            alt="branch"
+                                        />
+                                    </span>
+                                    <span>18</span>
+                                </div>
                             </div>
-                            <div>bar</div>
-                            <div>lang</div>
+                            <div className={styles.langArea}>
+                                <div className={styles.langBar}>
+                                    <span className={styles.langBar_inner}>
+                                        <span></span><span></span><span></span>
+                                    </span>
+                                </div>
+                                <div className={styles.langText}>
+                                    <span className={styles.lang}><span>TypeScript</span><span className={styles.ratio}>65.5%</span></span>
+                                    <span className={styles.lang}><span>JavaScript</span><span className={styles.ratio}>33.7%</span></span>
+                                    <span className={styles.lang}><span>Other</span><span className={styles.ratio}>0.8%</span></span>
+                                </div>
+                            </div>
                         </a>
                     </li>
                     <li>
                         <a href="#">
                             <h3>lightsound/nexst-tailwind</h3>
                             <p>Next.js starter template.</p>
-                            <div>
-                                <span>★</span><span>117</span><span>★</span><span>18</span>
+                            <div className={styles.icons}>
+                                <div className={styles.icon}>
+                                    <span>
+                                        <Image
+                                            src={StarImg}
+                                            alt="star"
+                                        />
+                                    </span>
+                                    <span>117</span>
+                                </div>
+                                <div className={styles.icon}>
+                                    <span>
+                                        <Image
+                                            src={BranchImg}
+                                            alt="branch"
+                                        />
+                                    </span>
+                                    <span>18</span>
+                                </div>
                             </div>
-                            <div>bar</div>
-                            <div>lang</div>
+                            <div className={styles.langArea}>
+                                <div className={styles.langBar}>
+                                    <span className={styles.langBar_inner}>
+                                        <span></span><span></span><span></span>
+                                    </span>
+                                </div>
+                                <div className={styles.langText}>
+                                    <span className={styles.lang}><span>TypeScript</span><span className={styles.ratio}>65.5%</span></span>
+                                    <span className={styles.lang}><span>JavaScript</span><span className={styles.ratio}>33.7%</span></span>
+                                    <span className={styles.lang}><span>Other</span><span className={styles.ratio}>0.8%</span></span>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <h3>lightsound/nexst-tailwind</h3>
+                            <p>Next.js starter template.</p>
+                            <div className={styles.icons}>
+                                <div className={styles.icon}>
+                                    <span>
+                                        <Image
+                                            src={StarImg}
+                                            alt="star"
+                                        />
+                                    </span>
+                                    <span>117</span>
+                                </div>
+                                <div className={styles.icon}>
+                                    <span>
+                                        <Image
+                                            src={BranchImg}
+                                            alt="branch"
+                                        />
+                                    </span>
+                                    <span>18</span>
+                                </div>
+                            </div>
+                            <div className={styles.langArea}>
+                                <div className={styles.langBar}>
+                                    <span className={styles.langBar_inner}>
+                                        <span></span><span></span><span></span>
+                                    </span>
+                                </div>
+                                <div className={styles.langText}>
+                                    <span className={styles.lang}><span>TypeScript</span><span className={styles.ratio}>65.5%</span></span>
+                                    <span className={styles.lang}><span>JavaScript</span><span className={styles.ratio}>33.7%</span></span>
+                                    <span className={styles.lang}><span>Other</span><span className={styles.ratio}>0.8%</span></span>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <h3>lightsound/nexst-tailwind</h3>
+                            <p>Next.js starter template.</p>
+                            <div className={styles.icons}>
+                                <div className={styles.icon}>
+                                    <span>
+                                        <Image
+                                            src={StarImg}
+                                            alt="star"
+                                        />
+                                    </span>
+                                    <span>117</span>
+                                </div>
+                                <div className={styles.icon}>
+                                    <span>
+                                        <Image
+                                            src={BranchImg}
+                                            alt="branch"
+                                        />
+                                    </span>
+                                    <span>18</span>
+                                </div>
+                            </div>
+                            <div className={styles.langArea}>
+                                <div className={styles.langBar}>
+                                    <span className={styles.langBar_inner}>
+                                        <span></span><span></span><span></span>
+                                    </span>
+                                </div>
+                                <div className={styles.langText}>
+                                    <span className={styles.lang}><span>TypeScript</span><span className={styles.ratio}>65.5%</span></span>
+                                    <span className={styles.lang}><span>JavaScript</span><span className={styles.ratio}>33.7%</span></span>
+                                    <span className={styles.lang}><span>Other</span><span className={styles.ratio}>0.8%</span></span>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <h3>lightsound/nexst-tailwind</h3>
+                            <p>Next.js starter template.</p>
+                            <div className={styles.icons}>
+                                <div className={styles.icon}>
+                                    <span>
+                                        <Image
+                                            src={StarImg}
+                                            alt="star"
+                                        />
+                                    </span>
+                                    <span>117</span>
+                                </div>
+                                <div className={styles.icon}>
+                                    <span>
+                                        <Image
+                                            src={BranchImg}
+                                            alt="branch"
+                                        />
+                                    </span>
+                                    <span>18</span>
+                                </div>
+                            </div>
+                            <div className={styles.langArea}>
+                                <div className={styles.langBar}>
+                                    <span className={styles.langBar_inner}>
+                                        <span></span><span></span><span></span>
+                                    </span>
+                                </div>
+                                <div className={styles.langText}>
+                                    <span className={styles.lang}><span>TypeScript</span><span className={styles.ratio}>65.5%</span></span>
+                                    <span className={styles.lang}><span>JavaScript</span><span className={styles.ratio}>33.7%</span></span>
+                                    <span className={styles.lang}><span>Other</span><span className={styles.ratio}>0.8%</span></span>
+                                </div>
+                            </div>
                         </a>
                     </li>
                 </ul>
