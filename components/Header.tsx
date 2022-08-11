f19003f3168ca13b24c4394f5416bde9ccebbef4
emih <emi.programming@gmail.com>
2022-08-11 12:41:46 +0900

    ✨ add toggle menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,37 +2,48 @@ import { Burger, Button, MediaQuery } from "@mantine/core"
 import { useState } from 'react';
 import { useToggle } from '@mantine/hooks';
 import styles from '../styles/Header.module.css'
+import Link from "next/link";
 
 export const Header = () => {
     const [opened, setOpened] = useState(false);
     const title = opened ? 'Close navigation' : 'Open navigation';
+    const BurgerOpenClass = opened ? styles.close : '';
+    const navOpenStyle = opened ? styles.open : '';
+    const BurgerFixed = opened ? styles.fixed : '';
     const [value, toggle] = useToggle(['🌙', '☀️']);
 
     return (
         <header className={styles.header}>
             <div className={styles.header_inner}>
                 <MediaQuery largerThan="xs" styles={{ display: 'none' }}>
-                    <Burger
-                        opened={opened}
-                        onClick={() => setOpened((o) => !o)}
-                        title={title}
-                    />
+                    <div className={`${styles.btnBurger} ${BurgerFixed}`}>
+                        <Burger
+                            opened={opened}
+                            onClick={() => setOpened((o) => !o)}
+                            title={title}
+                            className={BurgerOpenClass}
+                            size={18}
+                            color={opened ? "#fff" : ""}
+                        />
+                    </div>
                 </MediaQuery>
-                <h1 className={styles.logo}>Emi Portfolio</h1>
-                <nav className={styles.nav}>
-                    <ul>
-                        <li><a href="#about">about</a></li>
-                        <li><a href="#blog">blog</a></li>
-                        <li><a href="#portfolio">portfolio</a></li>
-                        <li><a href="#contact">contact</a></li>
-                    </ul>
+                <h1 className={styles.logo}><Link href="/"><a>Emi Portfolio</a></Link></h1>
+                <div className={styles.navArea}>
+                    <nav className={`${styles.nav} ${navOpenStyle}`}>
+                        <ul>
+                            <li><Link href="/about"><a>about</a></Link></li>
+                            <li><Link href="/blog"><a>blog</a></Link></li>
+                            <li><Link href="/portfolio"><a>portfolio</a></Link></li>
+                            <li><Link href="/contact"><a>contact</a></Link></li>
+                        </ul>
+                    </nav>
                     <div className={styles.btn_toggle}>
                         <Button variant="default" radius="xs" size="xs" onClick={() => toggle()}>
                             {value}
                         </Button>
                     </div>
-                </nav>
+                </div>
             </div>
-        </header>
+        </header >
     )
 }
