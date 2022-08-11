653896a36ed590bb99c45dfc56c7cfe620137b6e
emih <emi.programming@gmail.com>
2022-08-11 12:42:12 +0900

    ✨ add SNS links

--- components/Mv.tsx
+++ components/Mv.tsx
@@ -1,12 +1,51 @@
+import Image from 'next/image'
 import styles from '../styles/Mv.module.css'
+import iconTW from '../public/i_twitter.svg'
+import iconFB from '../public/i_fb.svg'
+import iconLines from '../public/i_lines.svg'
+import Link from 'next/link'
 
 export const Mv = () => {
     return (
         <div className={styles.mv}>
             <div className={styles.mv_inner}>
-                <h2>Emi Portfolio</h2>
-                <p>Emiのポートフォリオのためのページです</p>
-            </div>
+                <div>
+                    <h2>Emi Portfolio</h2>
+                    <p>Emiのポートフォリオのためのページです</p>
+                </div>
+                <div>
+                    <ul className={styles.iconList}>
+                        <li>
+                            <Link href="https://twitter.com/emi11882681">
+                                <a>
+                                    <Image
+                                        src={iconTW}
+                                        alt="twitter"
+                                    />
+                                </a></Link>
+                        </li>
+                        <li>
+                            <Link href="https://twitter.com/emi11882681">
+                                <a>
+                                    <Image
+                                        src={iconFB}
+                                        alt="fb"
+                                    />
+                                </a></Link>
+                        </li>
+                        <li>
+                            <Link href="https://twitter.com/emi11882681">
+                                <a>
+                                    <Image
+                                        src={iconLines}
+                                        alt="lines"
+                                    />
+                                </a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div >
         </div>
     )
 }
