ec9c72f153d3a6e85d6e5ad01e803d6d08a2d70a
emih <emi.programming@gmail.com>
2022-08-11 12:42:34 +0900

    ✨ add page

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,19 @@
+import { Header } from "../../components/Header"
+import { Mv } from "../../components/Mv"
+import styles from '../../styles/Home.module.css'
+
+function Post() {
+    return (
+        <>
+            <Header />
+            <Mv />
+            <div className={styles.content}>
+                <div className={styles.content_inner}>
+                    <p>投稿詳細</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Post
