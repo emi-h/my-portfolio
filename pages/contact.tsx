956112ee5e454a95ebf920d1820d1ae5b8a6e93a
emih <emi.programming@gmail.com>
2022-08-12 11:51:02 +0900

    💄 add contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import styles from '../styles/Home.module.css'
+import Form from "../components/Form";
 
 const Contact: NextPage = () => {
     return (
@@ -11,7 +12,10 @@ const Contact: NextPage = () => {
                 <main className={styles.main}>
                     <div className={styles.content}>
                         <div className={styles.content_inner}>
-                            <p>準備中</p>
+                            <section>
+                                <h2>contact</h2>
+                                <Form />
+                            </section>
                         </div>
                     </div>
                 </main>
