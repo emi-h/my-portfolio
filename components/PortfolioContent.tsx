5d1f0987f8cf8ea1ab1459db91a0f84954933f75
emih <emi.programming@gmail.com>
2022-08-12 12:24:47 +0900

    ♻️ add objectfit

--- components/PortfolioContent.tsx
+++ components/PortfolioContent.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image'
 import { FC } from 'react'
 import ThumbnailImg from '../public/test_thumbnail.jpg'
+import ThumbnailImg2 from '../public/test_thumbnail2.jpg'
 import styles from '../styles/Portfolio.module.css'
 import Btn from './Btn'
 
@@ -17,6 +18,25 @@ const PortfolioContent: FC = () => {
                                 <Image
                                     src={ThumbnailImg}
                                     alt="thumbnail"
+                                    layout='fill'
+                                    objectFit='cover'
+                                />
+                            </figure>
+                            <div>
+                                <h3>IT KINGDOM</h3>
+                                <p>当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。</p>
+                                <time dateTime='2021.10 - 2021.12'>2021.10 - 2021.12</time>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a className={styles.portfolioCard} href="#">
+                            <figure>
+                                <Image
+                                    src={ThumbnailImg2}
+                                    alt="thumbnail"
+                                    layout='fill'
+                                    objectFit='cover'
                                 />
                             </figure>
                             <div>
@@ -32,6 +52,8 @@ const PortfolioContent: FC = () => {
                                 <Image
                                     src={ThumbnailImg}
                                     alt="thumbnail"
+                                    layout='fill'
+                                    objectFit='cover'
                                 />
                             </figure>
                             <div>
@@ -47,6 +69,8 @@ const PortfolioContent: FC = () => {
                                 <Image
                                     src={ThumbnailImg}
                                     alt="thumbnail"
+                                    layout='fill'
+                                    objectFit='cover'
                                 />
                             </figure>
                             <div>
@@ -62,21 +86,8 @@ const PortfolioContent: FC = () => {
                                 <Image
                                     src={ThumbnailImg}
                                     alt="thumbnail"
-                                />
-                            </figure>
-                            <div>
-                                <h3>IT KINGDOM</h3>
-                                <p>当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。</p>
-                                <time dateTime='2021.10 - 2021.12'>2021.10 - 2021.12</time>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a className={styles.portfolioCard} href="#">
-                            <figure>
-                                <Image
-                                    src={ThumbnailImg}
-                                    alt="thumbnail"
+                                    layout='fill'
+                                    objectFit='cover'
                                 />
                             </figure>
                             <div>
