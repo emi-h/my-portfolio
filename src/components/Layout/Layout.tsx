495baedb4ce7d70ebba940ebf03a82f9095e035e
emih <emi.programming@gmail.com>
2022-08-16 20:47:55 +0900

    🔥 remove unnecessary files

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ReactElement } from "react";
 import { Header } from "src/components/Header/Header";
 import { Footer } from "src/components/Footer/Footer";
-import styles from "src/components/Layout.module.css";
+import styles from "src/components/Layout/Layout.module.css";
 
 type LayoutProps = Required<{
   readonly children: ReactElement;
