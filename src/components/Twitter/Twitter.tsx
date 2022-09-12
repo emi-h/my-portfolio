be7619b3077434a45adc92b63e88a2c9a3738fef
emih <emi.programming@gmail.com>
2022-09-12 21:04:43 +0900

    ✨ add twitter API

--- src/components/Twitter/Twitter.tsx
+++ src/components/Twitter/Twitter.tsx
@@ -1,111 +1,62 @@
 import { Avatar } from "@mantine/core";
-import Link from "next/link";
+import dayjs from "dayjs";
 import React from "react";
 import { FC } from "react";
+import useSWR from "swr";
+import {
+  findUserByUsername,
+  TwitterResponse,
+  usersIdTweets,
+} from "twitter-api-sdk/dist/types";
 
 import { Btn } from "../Btn/Btn";
 import styles from "./Twitter.module.css";
 
 export const Twitter: FC = () => {
+  const { data } = useSWR<{
+    tweets: TwitterResponse<usersIdTweets>["data"];
+    user: TwitterResponse<findUserByUsername>["data"];
+  }>(`/api/tweet`, async (url) => (await fetch(url)).json());
+  // console.log(data);
   return (
     <section className={styles.twitter} id="twitter">
       <h2>twitter</h2>
       <div className={styles.twitter_body}>
         <div className={styles.twitter_box}>
           <ul className={styles.twitter_list}>
-            <li>
-              <Link href="#">
-                <a>
+            {data?.tweets?.map((tweet) => {
+              return (
+                <li key={tweet.id}>
+                  {/* リンクの取得は難しそう？ */}
+                  {/* <Link href="#">
+                    <a> */}
                   <div className={styles.tweet_inner}>
                     <div className={styles.tweet_icon}>
                       <Avatar
                         radius="xl"
-                        src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
+                        src={data.user?.profile_image_url}
+                        alt={data.user?.name}
                       />
                     </div>
                     <div className={styles.tweet_body}>
                       <div className={styles.tweet_head}>
                         <span className={styles.tweet_name01}>
-                          しまぶーのIT大学
+                          {data?.user?.name}
                         </span>
                         <span className={styles.tweet_name02}>
-                          @shimabu_it・5月25日
+                          {`@${data?.user?.username}・${dayjs(
+                            tweet.created_at
+                          ).format("M月D日")}`}
                         </span>
                       </div>
-                      <p>
-                        📣 新サービス「Noway Form」をリリースしました！ Noway
-                        Formは、Notionのデータベースをもとにフォームを作成できるサービスです。これまでGoogle
-                        FormsでやっていたことがNotionだけで完結します✌✨
-                        試しに使っていただけると幸いです😊
-                        https://www.noway-form.com/ja
-                      </p>
+                      <p>{tweet.text}</p>
                     </div>
                   </div>
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <a>
-                  <div className={styles.tweet_inner}>
-                    <div className={styles.tweet_icon}>
-                      <Avatar
-                        radius="xl"
-                        src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
-                      />
-                    </div>
-                    <div className={styles.tweet_body}>
-                      <div className={styles.tweet_head}>
-                        <span className={styles.tweet_name01}>
-                          しまぶーのIT大学
-                        </span>
-                        <span className={styles.tweet_name02}>
-                          @shimabu_it・5月25日
-                        </span>
-                      </div>
-                      <p>
-                        📣 新サービス「Noway Form」をリリースしました！ Noway
-                        Formは、Notionのデータベースをもとにフォームを作成できるサービスです。これまでGoogle
-                        FormsでやっていたことがNotionだけで完結します✌✨
-                        試しに使っていただけると幸いです😊
-                        https://www.noway-form.com/ja
-                      </p>
-                    </div>
-                  </div>
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <a>
-                  <div className={styles.tweet_inner}>
-                    <div className={styles.tweet_icon}>
-                      <Avatar
-                        radius="xl"
-                        src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
-                      />
-                    </div>
-                    <div className={styles.tweet_body}>
-                      <div className={styles.tweet_head}>
-                        <span className={styles.tweet_name01}>
-                          しまぶーのIT大学
-                        </span>
-                        <span className={styles.tweet_name02}>
-                          @shimabu_it・5月25日
-                        </span>
-                      </div>
-                      <p>
-                        📣 新サービス「Noway Form」をリリースしました！ Noway
-                        Formは、Notionのデータベースをもとにフォームを作成できるサービスです。これまでGoogle
-                        FormsでやっていたことがNotionだけで完結します✌✨
-                        試しに使っていただけると幸いです😊
-                        https://www.noway-form.com/ja
-                      </p>
-                    </div>
-                  </div>
-                </a>
-              </Link>
-            </li>
+                  {/* </a>
+                  </Link> */}
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
---
+++ src/pages/api/tweet.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Client } from "twitter-api-sdk";
+import type {
+  findUserByUsername,
+  TwitterResponse,
+  usersIdTweets,
+} from "twitter-api-sdk/dist/types";
+// [改善が必要な点]
+// ・テキスト情報だけでなく、リンクや画像も取得したい。
+// ・expansionしてuser.fieldsを一緒に取得したい。
+// ・ツイート詳細のリンクを取得・設置したい。
+const handler = async (
+  _req: NextApiRequest,
+  res: NextApiResponse<{
+    tweets: TwitterResponse<usersIdTweets>["data"];
+    user: TwitterResponse<findUserByUsername>["data"];
+  }>
+) => {
+  const client = new Client(process.env.BEARER_TOKEN);
+  const { data: user } = await client.users.findUserByUsername("shimabu_it", {
+    "user.fields": ["created_at", "name", "profile_image_url", "username"],
+  });
+
+  if (!user) {
+    return;
+  }
+  const { data: tweets } = await client.tweets.usersIdTweets(user.id, {
+    max_results: 5,
+    "tweet.fields": ["author_id", "created_at", "geo"],
+    //     expansions: ["author_id"], //expansionしてuser.fieldsを一緒に取得するのが理想
+    //     "user.fields": ["profile_image_url", "username", "name"],
+  });
+
+  if (!tweets) {
+    return;
+  }
+  res.status(200).json({ tweets, user });
+};
+
+export default handler;
